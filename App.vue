ae979c733afe5bfbed19e83a6aeb4dab3f16eed5
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-09 23:59:29 +0300

    Added input, display data

--- App.vue
+++ App.vue
@@ -2,8 +2,20 @@
   <nb-container v-if="isLoaded">
     <image-background :source="image" class="image-container">
       <nb-container class="custom-container">
-        <display />
-        <search />
+        <view class="text-container" v-if="weather">
+          <text class="location" v-if="weather.sys">{{ weather.name }}, {{ weather.sys.country }}</text>
+          <text class="temp" v-if="weather.main">{{weather.main.temp}}</text>
+        </view>
+        <view class="text-container" v-if="weather.message">
+          <text class="temp">{{weather.message}}</text>
+        </view>
+        <nb-item class="custom-item">
+          <nb-icon active name="search" />
+          <nb-input placeholder="Search" v-model="searchContent" class="custom-input" />
+        </nb-item>
+        <nb-button :onPress="handleSubmit" rounded class="custom-btn">
+          <nb-text>Search</nb-text>
+        </nb-button>
       </nb-container>
     </image-background>
   </nb-container>
@@ -16,22 +28,23 @@ import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import summer from "./assets/summer.jpg";
 import winter from "./assets/winter.jpg";
-import Search from "./components/Search/Search";
-import Display from "./components/Display/Display";
-
+import { searchInfoByLocation } from "./services/weather.service";
 Vue.use(VueNativeBase);
 
 export default {
   name: "App",
-  components: {
-    Search,
-    Display
-  },
   data: function() {
     return {
       isLoaded: false,
-      image: summer
+      image: summer,
+      searchContent: "",
+      weather: {}
     };
+  },
+  methods: {
+    async handleSubmit() {
+      this.weather = await searchInfoByLocation(this.searchContent);
+    }
   },
   mounted: async function() {
     await Font.loadAsync({
@@ -53,6 +66,29 @@ export default {
   width: 80%;
   margin: auto;
   left: 10%;
+  justify-content: center;
+}
+
+.text-container {
+    margin-top: 0%;
+    margin-bottom: 50%;
+  }
+
+  .location {
+    text-align: center;
+    font-size: 20px;
+  }
+
+  .temp {
+    text-align: center;
+    font-size: 50px;
+  }
+
+  .custom-btn {
+  width: 50%;
+  margin-top: 10px;
+  margin-left: auto;
+  margin-right: auto;
   justify-content: center;
 }
 </style>
