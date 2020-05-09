bf6366ab5752b36c16ba702053c2af37d49780fe
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-09 19:57:40 +0200

    Added search

--- App.vue
+++ App.vue
@@ -1,17 +1,61 @@
 <template>
-  <view class="container">
-    <text class="text-color-primary">Wow</text>
-  </view>
+  <nb-container v-if="isLoaded" class="custom-container">
+      <nb-item class="custom-item">
+        <nb-icon active name="search" />
+        <nb-input placeholder="Search" v-model="searchContent" class="custom-input"/>
+      </nb-item>
+      <nb-button v-bind:onPress="handleSubmit" rounded class="custom-btn">
+          <nb-text>Search</nb-text>
+      </nb-button>
+  </nb-container>
 </template>
 
+<script>
+import Vue from "vue-native-core";
+import { VueNativeBase } from "native-base";
+import * as Font from "expo-font";
+import { Ionicons } from '@expo/vector-icons';
+
+Vue.use(VueNativeBase);
+
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      isLoaded: false,
+      searchContent: "",
+      content: ""
+    }
+  },
+  mounted: async function() {
+    await Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf')
+    });
+    this.isLoaded = true;
+  },
+  methods: {
+    handleSubmit() {
+      this.content = this.searchContent;
+    }
+  }
+};
+</script>
+
 <style>
-.container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.text-color-primary {
-  color: blue;
-}
+  .custom-container {
+    width: 80%;
+    margin: auto;
+    left: 10%;
+    justify-content: center;
+  }
+
+  .custom-btn {
+    width: 50%;
+    margin-top: 10px;
+    margin-left: auto;
+    margin-right: auto;
+    justify-content: center;
+  }
+
 </style>
