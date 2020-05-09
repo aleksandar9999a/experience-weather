0530436503a07987528067e51a572b4beabb313e
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-09 22:29:12 +0300

    Remove name, added location and temp

--- components/Display/Display.vue
+++ components/Display/Display.vue
@@ -1,11 +1,28 @@
 <template>
+  <view class="text-container">
+    <text class="location">Sofia</text>
+    <text class="temp">50C</text>
+  </view>
 </template>
 
 <script>
 export default {
-  name: "wDisplay"
 };
 </script>
 
 <style>
+  .text-container {
+    margin-top: 0%;
+    margin-bottom: 50%;
+  }
+
+  .location {
+    text-align: center;
+    font-size: 20px;
+  }
+
+  .temp {
+    text-align: center;
+    font-size: 50px;
+  }
 </style>
