b245ceaf25fd1245a31e33679028d83e0bd15493
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-09 22:10:05 +0300

    Move input to search component

--- App.vue
+++ App.vue
@@ -1,15 +1,7 @@
 <template>
   <nb-container v-if="isLoaded">
     <image-background :source="image" class="image-container">
-      <nb-container class="custom-container">
-        <nb-item class="custom-item">
-          <nb-icon active name="search" />
-          <nb-input placeholder="Search" v-model="searchContent" class="custom-input" />
-        </nb-item>
-        <nb-button :onPress="handleSubmit" rounded class="custom-btn">
-          <nb-text>Search</nb-text>
-        </nb-button>
-      </nb-container>
+      <w-search />
     </image-background>
   </nb-container>
 </template>
@@ -21,16 +13,16 @@ import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import summer from "./assets/summer.jpg";
 import winter from "./assets/winter.jpg";
+import wSearch from "./components/Search/Search";
 
 Vue.use(VueNativeBase);
 
 export default {
   name: "App",
+  components: { wSearch },
   data: function() {
     return {
       isLoaded: false,
-      searchContent: "",
-      content: "",
       image: summer
     };
   },
@@ -40,15 +32,6 @@ export default {
       Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
     });
     this.isLoaded = true;
-  },
-  methods: {
-    handleSubmit() {
-      if (this.image === winter) {
-        this.image = summer;
-      } else {
-        this.image = winter;
-      }
-    }
   }
 };
 </script>
@@ -56,20 +39,5 @@ export default {
 <style>
 .image-container {
   flex: 1;
-}
-.custom-container {
-  background-color: transparent;
-  width: 80%;
-  margin: auto;
-  left: 10%;
-  justify-content: center;
-}
-
-.custom-btn {
-  width: 50%;
-  margin-top: 10px;
-  margin-left: auto;
-  margin-right: auto;
-  justify-content: center;
 }
 </style>
---
+++ components/Search/Search.vue
@@ -0,0 +1,43 @@
+<template>
+  <nb-container class="custom-container">
+    <nb-item class="custom-item">
+      <nb-icon active name="search" />
+      <nb-input placeholder="Search" v-model="searchContent" class="custom-input" />
+    </nb-item>
+    <nb-button :onPress="handleSubmit" rounded class="custom-btn">
+      <nb-text>Search</nb-text>
+    </nb-button>
+  </nb-container>
+</template>
+
+<script>
+export default {
+  name: "wSearch",
+  data: function() {
+    return {
+      searchContent: ""
+    };
+  },
+  methods: {
+    handleSubmit() {}
+  }
+};
+</script>
+
+<style>
+.custom-container {
+  background-color: transparent;
+  width: 80%;
+  margin: auto;
+  left: 10%;
+  justify-content: center;
+}
+
+.custom-btn {
+  width: 50%;
+  margin-top: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  justify-content: center;
+}
+</style>
